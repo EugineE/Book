316e9f873ef1c6f6098fdd36a14e4a550b3cb7a1
EugineE <eeejerish@gmail.com>
2017-07-03 12:30:55 +0530

    Add files via upload

---
+++ PubHub100-310814205027-core/src/com/revature/util/TestConnection.java
@@ -0,0 +1,13 @@
+package com.revature.util;
+
+import java.sql.SQLException;
+
+public class TestConnection {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		System.out.println(ConnectionUtil.getConnection());
+
+	}
+
+}
